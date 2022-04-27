ca4202c29524f8d098b87e20fe7997c74e141eb8
jdacosta1991 <workdacojo@gmail.com>
2022-04-27 11:21:51 -0400

    Add auth_token with SecureRandom.hex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
-    has_many :posts
-    validates :name, presence: true
-    validates :email, presence: true
-    validates :auth_token, presence: true
+  
+  has_many :posts
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :auth_token, presence: true
+
+  after_initialize :generate_auth_token
+
+  def generate_auth_token
+    # User.new
+    # if !auth_token.present?
+    unless auth_token.present? 
+    # generate token
+      self.auth_token = TokenGenerationService.generate
+    end      
+  end
 end
